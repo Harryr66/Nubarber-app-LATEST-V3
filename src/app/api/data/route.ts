d5fe5e400ccdbd98b6b07f375b8b803f66ba3d86
Harry Rollerson <harry@Harrys-MacBook-Pro.local>
2025-08-18 00:57:06 +0800

    Make API route dynamic to fix build error

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { NextResponse } from "next/server";
 import { getFirestore } from "firebase/firestore";
 import { app } from "../../../firebase"; // Adjust path if needed
